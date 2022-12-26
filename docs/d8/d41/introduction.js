0bc37e64fc1ad369119c5bede8e19ed157cef265
ButterSus <eduardkrivosapkin5@gmail.com>
2022-12-27 02:43:17 +0900

    added

---
+++ docs/d8/d41/introduction.js
@@ -0,0 +1,10 @@
+var introduction =
+[
+    [ "Язык описания датапаков Kiwi", "dd/d16/introduction_index.html", [
+      [ "Ознакомление", "dd/d16/introduction_index.html#autotoc_md0", null ],
+      [ "Принцип работы", "dd/d16/introduction_index.html#autotoc_md1", null ]
+    ] ],
+    [ "Установка и первый датапак на Kiwi", "d8/d3b/introduction_installation.html", [
+      [ "Иерархия файлов в проекте", "d8/d3b/introduction_installation.html#autotoc_md2", null ]
+    ] ]
+];
